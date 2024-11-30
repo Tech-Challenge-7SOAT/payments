502952071c6972d061b152bc3be93a4d6bb3c86e
Gabriel Mariusso Caldeira <gabriel_mariusso@hotmail.com>
2024-11-30 20:12:44 -0300

    adding mongo conection

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	implementation("org.mongodb:mongodb-driver-sync:4.11.0")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
